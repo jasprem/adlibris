71f255964c298f2e2fbea6666a7f88b2ae0f56fb
prem <jasprem1987@gmail.com>
2018-03-21 13:57:22 +0100

    Add test framework

---
+++ tests/Catalog.WebApi.IntegrationTests/TestContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+
+namespace Catalog.WebApi.IntegrationTests
+{
+    public class TestContext
+    {
+        private TestServer _server;
+        public HttpClient Client { get; private set; }
+
+        public TestContext()
+        {
+            SetUpClient();
+        }
+
+        private void SetUpClient()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+
+            Client = _server.CreateClient();
+        }
+    }
+}
